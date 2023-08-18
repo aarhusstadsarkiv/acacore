6c835683ef13e4f9a9bf81c48231d6900a435bb4
Matteo Campinoti <matteo.campinoti94@gmail.com>
2023-08-18 13:10:49 +0200

    models - use newer version File from digiarch

--- acacore/models/file.py
+++ acacore/models/file.py
@@ -1,115 +1,123 @@
 # -----------------------------------------------------------------------------
 # Imports
 # -----------------------------------------------------------------------------
-import re
-from pathlib import Path
-from typing import Any
-from typing import Dict
-from typing import Optional
-from typing import Pattern
-from uuid import uuid4
 
-from pydantic import Field
-from pydantic import UUID4
-from pydantic import validator
+
+import os
+from pathlib import Path
+from typing import Optional
+from uuid import UUID, uuid4
+
+from pydantic import UUID4, Field, validator
 
 from .base import ACABase
 from .identification import Identification
 from ..utils.io import size_fmt
 
 
-# noinspection PyNestedDecorators
-class File(ACABase):
-    """File data model"""
+# -----------------------------------------------------------------------------
+# Model
+# -----------------------------------------------------------------------------
 
-    path: Path
+
+class File(ACABase):
+    """File data model."""
+
     uuid: UUID4 = Field(None)
     checksum: Optional[str]
-    aars_path: Path = Field(None)
+    relative_path: Path = Field(None)
+    is_binary: bool = Field(None)
+    file_size_in_bytes: int = Field(None)
 
     # Validators
-    @validator("path")
-    @classmethod
+    @validator("relative_path")
     def path_must_be_file(cls, path: Path) -> Path:
-        """Resolves the file path and validates that it points
-        to an existing file."""
-        if not path.resolve().is_file():
-            raise ValueError("File does not exist")
-        return path.resolve()
+        """Resolves the file path and validates that it points to an existing file."""
+        if "ROOTPATH" in os.environ:
+            if os.name == "posix":
+                posix_path = str(path).replace("\\", "/")
+                absolute_path = Path(os.environ["ROOTPATH"], posix_path)
+                # print(absolute_path)
+            else:
+                absolute_path = Path(os.environ["ROOTPATH"], path)
+            if not absolute_path.resolve().is_file():
+                raise ValueError(f"File with path {absolute_path} does not exist")
+        else:
+            if not path.resolve().is_file():
+                raise ValueError(f"File with path {path} does not exist")
+        if os.name == "posix":
+            return Path(str(path).replace("\\", "/"))
+        else:
+            return path
 
     @validator("uuid", pre=True, always=True)
-    @classmethod
-    def set_uuid(cls, uuid: UUID4) -> UUID4:
+    def set_uuid(cls, uuid: UUID4) -> UUID:
         return uuid or uuid4()
 
-    @validator("aars_path", pre=True, always=True)
-    @classmethod
-    def set_aars_path(cls, aars_path: Path, values: Dict[str, Any]) -> Path:
-        path: Optional[Path] = values.get("path")
-        if path:
-            path_parts = list(path.parts)
-        else:
-            path_parts = []
-        root_regex: Pattern = re.compile(r"(?i)aars\.")
-        for part in path_parts:
-            if root_regex.search(part):
-                root_index = path_parts.index(part)
-                return Path(*path_parts[root_index:])
-        else:
-            raise ValueError(
-                "Unable to parse AARS path, please check your directory naming"
-            )
+    def get_absolute_path(self) -> Path:
+        return Path(os.environ["ROOTPATH"], self.relative_path)
 
     def read_text(self) -> str:
-        """Expose read_text() functionality from pathlib.
+        """Expose read text functionality from pathlib.
+
         Encoding is set to UTF-8.
 
-        Returns
+        Returns:
         -------
         str
             File text data.
         """
-        return self.path.read_text(encoding="utf-8")
+        return self.get_absolute_path().read_text(encoding="utf-8")
 
     def read_bytes(self) -> bytes:
         """Expose read_bytes() functionality from pathlib.
 
-        Returns
+        Returns:
         -------
         bytes
             File byte data.
-        """
-        return self.path.read_bytes()
+        """  # noqa: D402
+        return self.get_absolute_path().read_bytes()
 
     def name(self) -> str:
         """Get the file name.
 
-        Returns
+        Returns:
         -------
         str
             File name.
         """
-        return self.path.name
+        return self.relative_path.name
 
     def ext(self) -> str:
         """Get the file extension.
 
-        Returns
+        Returns:
         -------
         str
             File extension.
         """
-        return self.path.suffix.lower()
+        return self.relative_path.suffix.lower()
 
     def size(self) -> str:
-        """Get the file size in human-readable string format.
+        """Get the file size in human readable string format.
 
-        Returns
+        Returns:
         -------
         str
-            File size in human-readable format.
+            File size in human readable format.
         """
-        return size_fmt(self.path.stat().st_size)
+        return str(size_fmt(self.get_absolute_path().stat().st_size))
+
+    def size_as_int(self) -> int:
+        """Get the file size in human readable string format.
+
+        Returns:
+        -------
+        int
+            File size in human readable format.
+        """
+        return self.get_absolute_path().stat().st_size
 
 
 class ArchiveFile(Identification, File):
