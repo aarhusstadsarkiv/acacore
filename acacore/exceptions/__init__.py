f71883116b18a0bf3dd1d3c7f56446817f183dce
Matteo Campinoti <matteo.campinoti94@gmail.com>
2023-08-21 14:51:16 +0200

    __init__ - export sub-submodules

--- acacore/exceptions/__init__.py
+++ acacore/exceptions/__init__.py
@@ -1,4 +1,2 @@
-__all__ = [
-    "base",
-    "files"
-]
+from . import base
+from . import files
--- acacore/models/__init__.py
+++ acacore/models/__init__.py
@@ -1,1 +1,5 @@
-__all__ = ["file_data", "metadata"]
+from . import base
+from . import file
+from . import file_data
+from . import identification
+from . import metadata
