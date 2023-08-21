d56b04bf5fcda12860d4b49d7ff484442fd80264
Matteo Campinoti <matteo.campinoti94@gmail.com>
2023-08-21 14:45:15 +0200

    models:file - reduce to minimum requirements

--- acacore/models/file.py
+++ acacore/models/file.py
@@ -23,39 +23,18 @@ from ..utils.io import size_fmt
 class File(ACABase):
     """File data model."""
 
-    uuid: UUID4 = Field(None)
+    id: int
+    uuid: UUID4
     checksum: Optional[str]
-    relative_path: Path = Field(None)
-    is_binary: bool = Field(None)
-    file_size_in_bytes: int = Field(None)
+    puid: Optional[str]
+    relative_path: Path
+    is_binary: bool
+    file_size_in_bytes: int
+    signature: Optional[str]
+    warning: Optional[str]
 
-    # Validators
-    @validator("relative_path")
-    def path_must_be_file(cls, path: Path) -> Path:
-        """Resolves the file path and validates that it points to an existing file."""
-        if "ROOTPATH" in os.environ:
-            if os.name == "posix":
-                posix_path = str(path).replace("\\", "/")
-                absolute_path = Path(os.environ["ROOTPATH"], posix_path)
-                # print(absolute_path)
-            else:
-                absolute_path = Path(os.environ["ROOTPATH"], path)
-            if not absolute_path.resolve().is_file():
-                raise ValueError(f"File with path {absolute_path} does not exist")
-        else:
-            if not path.resolve().is_file():
-                raise ValueError(f"File with path {path} does not exist")
-        if os.name == "posix":
-            return Path(str(path).replace("\\", "/"))
-        else:
-            return path
-
-    @validator("uuid", pre=True, always=True)
-    def set_uuid(cls, uuid: UUID4) -> UUID:
-        return uuid or uuid4()
-
-    def get_absolute_path(self) -> Path:
-        return Path(os.environ["ROOTPATH"], self.relative_path)
+    def get_absolute_path(self, root: Optional[Path] = None) -> Path:
+        return root.joinpath(self.relative_path) if root else self.relative_path.resolve()
 
     def read_text(self) -> str:
         """Expose read text functionality from pathlib.
@@ -99,25 +78,25 @@ class File(ACABase):
         """
         return self.relative_path.suffix.lower()
 
-    def size(self) -> str:
-        """Get the file size in human readable string format.
-
-        Returns:
-        -------
-        str
-            File size in human readable format.
-        """
-        return str(size_fmt(self.get_absolute_path().stat().st_size))
-
-    def size_as_int(self) -> int:
-        """Get the file size in human readable string format.
+    def size(self) -> int:
+        """Get the file size in bytes
 
         Returns:
         -------
         int
-            File size in human readable format.
+            File size in bytes.
         """
         return self.get_absolute_path().stat().st_size
+
+    def size_fmt(self) -> str:
+        """Get the file size in a human-readable string format.
+
+        Returns:
+        -------
+        str
+            File size in human-readable format.
+        """
+        return str(size_fmt(self.get_absolute_path().stat().st_size))
 
 
 class ArchiveFile(Identification, File):
