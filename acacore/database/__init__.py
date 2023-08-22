1f3fe9e4ca6cb52f76bb746f7cc33a86ed40603f
Matteo Campinoti <matteo.campinoti94@gmail.com>
2023-08-22 12:58:32 +0200

    database - export model_to_columns

--- acacore/database/__init__.py
+++ acacore/database/__init__.py
@@ -2,4 +2,5 @@ from .files_db import Column
 from .files_db import FileDB
 from .files_db import SelectColumn
 from .files_db import Table
+from .files_db import model_to_columns
 from .files_db import or_none
