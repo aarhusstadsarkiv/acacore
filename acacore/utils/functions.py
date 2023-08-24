a809c03c97f675e7c0acc6fe6da4a635994845c8
Matteo Campinoti <matteo.campinoti94@gmail.com>
2023-08-24 09:05:35 +0200

    utils:functions - move or_none

---
+++ acacore/utils/functions.py
@@ -0,0 +1,20 @@
+from typing import Callable
+from typing import Optional
+from typing import TypeVar
+
+T = TypeVar("T")
+R = TypeVar("R")
+
+
+def or_none(func: Callable[[T], R]) -> Callable[[T], Optional[R]]:
+    """
+    Create a lambda function of arity one that will return None if its argument is None,
+    otherwise it will call func on the object.
+
+    Args:
+        func: A function of type (T) -> R that will handle the object if it is not none.
+
+    Returns:
+        object: A function of type (T) -> R | None.
+    """
+    return lambda x: None if x is None else func(x)
