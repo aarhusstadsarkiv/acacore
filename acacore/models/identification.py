cb4657dce7b969937f50c5290eddc7531f749e83
Matteo Campinoti <matteo.campinoti94@gmail.com>
2023-10-31 13:24:54 +0100

    models:identification - rename Identification.file_size_in_bytes to .size
    Match File.size

--- acacore/models/identification.py
+++ acacore/models/identification.py
@@ -12,7 +12,7 @@ class Identification(ACABase):
     puid: Optional[str]
     signature: Optional[str]
     warning: Optional[str]
-    file_size: Optional[int]
+    size: Optional[int]
 
     # noinspection PyNestedDecorators
     @model_validator(mode="before")
