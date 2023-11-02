96a1ddff782bbc86b9796b54e1bb9baf142e7582
Matteo Campinoti <matteo.campinoti94@gmail.com>
2023-11-02 07:56:54 +0100

    models:reference_files - convert models to new action format

--- acacore/models/reference_files.py
+++ acacore/models/reference_files.py
@@ -1,17 +1,9 @@
 """Data models for the data on saved to different .json files on the `reference_files` repo."""
+from typing import Literal
 from typing import Optional
 
 from pydantic import BaseModel
-from pydantic import field_validator
-
-
-class ReIdentifyModel(BaseModel):
-    """Data model for the `to_re-identify` from reference_files."""
-
-    puid: Optional[str] = None
-    name: Optional[str] = None
-    ext: Optional[str] = None
-    reasoning: Optional[str] = None
+from pydantic import Field
 
 
 class CustomSignature(BaseModel):
@@ -25,32 +17,96 @@ class CustomSignature(BaseModel):
     extension: Optional[str] = None
 
 
-class ConversionInstruction(BaseModel):
-    puid: str
+class ConvertAction(BaseModel):
+    """
+    Class representing an action to convert a file to a different format.
+
+    Attributes:
+        converter (str): The converter to use for the conversion.
+        outputs (list[str]): The list of file types to convert to.
+    """
+
     converter: str
-    outputs: list[str]
+    converter_type: Literal["master", "statutory", "access"]
+    outputs: list[str] = Field(min_items=1)
 
 
-class ManualConversionInstruction(BaseModel):
-    puid: str
-    reasoning: str
-    process: str
+class ExtractAction(BaseModel):
+    """
+    Class representing an action to extract data from a file.
 
+    Attributes:
+        tool (str): The name of the tool used for extraction.
+        dir_suffix (str): The output directory where the extracted data will be saved.
+    """
 
-class ExtractionInstruction(BaseModel):
-    puid: str
     tool: str
     dir_suffix: str
 
 
-class IgnoreInstruction(BaseModel):
-    puid: str
-    description: str = ""
-    extensions: list[str]
+class ReplaceAction(BaseModel):
+    """
+    Class representing a replacement action.
+
+    Attributes:
+        template (str): The replacement template.
+    """
+
+    template: str
+
+
+class ManualAction(BaseModel):
+    """
+    Class representing a manual action in a workflow.
+
+    Attributes:
+        reasoning (str): The reasoning behind the manual action.
+        process (str): The process for performing the manual action.
+    """
+
+    reasoning: str
+    process: str
+
+
+class IgnoreAction(BaseModel):
+    """
+    Class representing an action to ignore a specific file based on the given reasoning.
+
+    Attributes:
+        reasoning (str): The reasoning for ignoring the file.
+    """
+
     reasoning: str
 
-    # noinspection PyNestedDecorators
-    @field_validator("extensions", mode="before")
-    @classmethod
-    def extensions_validator(cls, data: str):
-        return list(filter(bool, map(str.strip, data.strip().split(","))))
+
+class ReIdentifyAction(BaseModel):
+    """
+    Class representing an action to ignore a specific file based on the given reasoning.
+
+    Attributes:
+        reasoning (str): The reasoning for ignoring the file.
+    """
+
+    reasoning: str
+    onfail: Optional[Literal["convert", "extract", "manual", "ignore"]] = None
+
+
+class RenameAction(BaseModel):
+    """
+    Class representing an action to rename a file. It is a dictionary-based class with the following fields.
+
+    Attributes:
+        new_name (str): A string representing the new name for the file.
+    """
+
+    new_name: str
+
+
+class Action(BaseModel):
+    name: str
+    action: Literal["convert", "extract", "manual", "ignore", "reidentify"]
+    convert: Optional[list[ConvertAction]] = None
+    extract: Optional[ExtractAction] = None
+    manual: Optional[ManualAction] = None
+    ignore: Optional[IgnoreAction] = None
+    reidentify: Optional[ReIdentifyAction] = None
