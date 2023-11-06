c02cce73ce8380df97d460e735247f007bdee26e
Matteo Campinoti <matteo.campinoti94@gmail.com>
2023-11-06 13:54:04 +0100

    models:history - set time default to datetime.now

--- acacore/models/history.py
+++ acacore/models/history.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from typing import Optional
 
+from pydantic import Field
 from pydantic import UUID4
 
 from .base import ACABase
@@ -8,7 +9,7 @@ from .base import ACABase
 
 class HistoryEntry(ACABase):
     uuid: Optional[UUID4] = None
-    time: datetime
+    time: datetime = Field(default_factory=datetime.now)
     operation: str
     data: Optional[object] = None
     reason: Optional[str] = None
