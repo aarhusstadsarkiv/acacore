0e492e47fc11a3a88bd3ce5ce9251b1eee8138ed
Matteo Campinoti <matteo.campinoti94@gmail.com>
2024-06-17 12:12:54 +0200

    models.history - use DBField, command_history can only take click.Context, use option to add params and acacore version to data

--- acacore/models/history.py
+++ acacore/models/history.py
@@ -1,16 +1,18 @@
 from datetime import datetime
 from typing import Optional
-from typing import Union
 
 from click import Context
 from pydantic import Field
 from pydantic import UUID4
 
+from acacore.__version__ import __version__
+from acacore.database.column import DBField
+
 from .base import ACABase
 
 
 class HistoryEntry(ACABase):
-    uuid: Optional[UUID4] = Field(default=None, index=["idx_uuid_history"])
+    uuid: Optional[UUID4] = DBField(default=None, index=["idx_uuid_history"])
     time: datetime = Field(default_factory=datetime.now)
     operation: str
     data: Optional[object] = None
@@ -19,44 +21,46 @@ class HistoryEntry(ACABase):
     @classmethod
     def command_history(
         cls,
-        ctx: Union[Context, str],
+        ctx: Context,
         operation: str,
         uuid: Optional[UUID4] = None,
-        data: Optional[object] = ...,
+        data: Optional[object] = None,
         reason: Optional[str] = None,
         time: Optional[datetime] = None,
+        add_params_to_data: bool = False,
     ) -> "HistoryEntry":
         """
         Create a HistoryEntry for a command.
 
         Args:
-            ctx: The context object representing the current command execution. Can be either a `click.Context` instance or a string.
+            ctx: The context object representing the current command execution.
             operation: The name of the operation for which the command history entry is being created.
             uuid: Optional. The UUID associated with the command history entry. Default is None.
             data: Optional. Additional data or parameters associated with the command history entry.
                 It defaults to the params property of the context, if it is a click.Context object, otherwise None.
             reason: Optional. The reason for the command execution. Default is None.
             time: Optional. The timestamp of the command execution. Default is None.
+            add_params_to_data: If true, add context parameters to data
 
         Returns:
             HistoryEntry: A `HistoryEntry` instance representing the command history entry.
         """
         command: str
-
-        if isinstance(ctx, Context):
-            current: Context = ctx
-            command_parts: list[str] = [current.command.name]
-            while current.parent is not None:
-                current = current.parent
-                command_parts.insert(0, current.command.name)
-            command = ".".join(command_parts)
-        else:
-            command = ctx
+        current: Context = ctx
+        command_parts: list[str] = []
+        while current:
+            command_parts.insert(0, current.command.name)
+            current = current.parent
+        command = ".".join(command_parts)
 
         operation = f"{command.strip(':.')}:{operation.strip(':')}"
 
-        if data is Ellipsis:
-            data = ctx.params if isinstance(ctx, Context) else None
+        if add_params_to_data and isinstance(data, dict):
+            data |= {"acacore": __version__, "params": ctx.params}
+        elif add_params_to_data and isinstance(data, list):
+            data.append({"acacore": __version__, "params": ctx.params})
+        elif add_params_to_data:
+            raise TypeError(f"Data type {type(data)} is not compatible with add_params_to_data")
 
         return cls(
             uuid=uuid,
