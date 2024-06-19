a8fd6a616f70a293b42e0ab2dff6441a84edefaa
Matteo Campinoti <matteo.campinoti94@gmail.com>
2024-06-19 10:55:39 +0200

    tests.linting - use acacore.__file__ as linting path

--- tests/linting.py
+++ tests/linting.py
@@ -1,10 +1,12 @@
 import subprocess
 from pathlib import Path
 
+from acacore import __file__ as acacore_init_file
+
 
 def test_black(test_folder: Path):
     completed_process = subprocess.run(
-        ["poetry", "run", "black", "--check", str(test_folder.parent)],
+        ["poetry", "run", "black", "--check", str(Path(acacore_init_file).parent)],
         check=False,
     )
     assert completed_process.returncode == 0
@@ -12,7 +14,7 @@ def test_black(test_folder: Path):
 
 def test_ruff(test_folder: Path):
     completed_process = subprocess.run(
-        ["poetry", "run", "ruff", "check", str(test_folder.parent)],
+        ["poetry", "run", "ruff", "check", str(Path(acacore_init_file).parent)],
         check=False,
     )
     assert completed_process.returncode == 0
