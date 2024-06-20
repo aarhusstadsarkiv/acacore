3402bbfbe04bb4d26937422254726b3ea545a6b1
Matteo Campinoti <matteo.campinoti94@gmail.com>
2024-06-20 11:07:16 +0200

    models.base:NoDefaultsModel - add base model that does not dump defaults

---
+++ acacore/models/base.py
@@ -0,0 +1,12 @@
+from typing import Any
+from typing import Callable
+from typing import Self
+
+from pydantic import BaseModel
+from pydantic import model_serializer
+
+
+class NoDefaultsModel(BaseModel):
+    @model_serializer(mode="wrap", when_used="always")
+    def _model_serializer(self, wrap: Callable[[Self], dict[str, Any]]) -> dict[str, Any]:
+        return {k: v for k, v in wrap(self).items() if k in self.model_fields_set and v != self.model_fields[k].default}
