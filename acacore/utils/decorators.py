792543d489ee376b0d4f4dd8234277cde267ba70
Matteo Campinoti <matteo.campinoti94@gmail.com>
2024-09-25 12:24:49 +0200

    utils.decorators:docstring_format - add docstring

--- acacore/utils/decorators.py
+++ acacore/utils/decorators.py
@@ -3,6 +3,12 @@ from typing import Callable
 
 
 def docstring_format(**kwargs: Any) -> Callable[[Callable], Callable]:  # noqa: ANN401
+    """
+    Format a docstring with keyword arguments.
+
+    :param kwargs: The parameter(s) to use to format the docstring.
+    """
+
     def decorator(func: Callable) -> Callable:
         func.__doc__ = (func.__doc__ or "").format(**kwargs)
         return func
