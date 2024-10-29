766910ef70636bc46eacd75df868bf6102e8fec7
Matteo Campinoti <matteo.campinoti94@gmail.com>
2024-10-29 12:44:00 +0100

    database.upgrade:upgrade_3_2to3_3 - fix incorrect column name

--- acacore/database/upgrade.py
+++ acacore/database/upgrade.py
@@ -219,8 +219,8 @@ def upgrade_3_1to3_2(conn: Connection) -> Version:
 def upgrade_3_2to3_3(conn: Connection) -> Version:
     if not conn.execute("select 1 from pragma_table_info('Files') where name = 'original_name'").fetchone():
         conn.execute("alter table Files add column original_name text not null default ''")
-    if not conn.execute("select 1 from pragma_table_info('Files') where name = 'processed_name'").fetchone():
-        conn.execute("alter table Files add column processed_name text default '[]'")
+    if not conn.execute("select 1 from pragma_table_info('Files') where name = 'processed_names'").fetchone():
+        conn.execute("alter table Files add column processed_names text default '[]'")
 
     def _find_original_name(uuid: str, relative_path: str) -> str:
         original_path: Path = Path(relative_path)
