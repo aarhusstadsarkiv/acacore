b23d7acbcd16ce1b5d6e0866889d95d10c9b1517
Matteo Campinoti <matteo.campinoti94@gmail.com>
2024-11-27 15:53:37 +0100

    database.column - add docstrings

--- acacore/database/column.py
+++ acacore/database/column.py
@@ -68,6 +68,16 @@ def _dump_object(obj: list | tuple | dict | BaseModel) -> list | dict:
 
 @dataclass
 class ColumnSpec:
+    """
+    Class representing a SQLite column.
+
+    :ivar name: The name of the column.
+    :ivar type: The SQLite type of the column.
+    :ivar nullable: Whether the column is nullable.
+    :ivar to_sql: A function that converts a value into an SQLIte-compatible type.
+    :ivar from_sql: A function that converts an SQLIte return value into a different one..
+    """
+
     name: str
     type: str
     to_sql: Callable[[Any | None], SQLValue]
@@ -78,10 +88,23 @@ class ColumnSpec:
         return f"{self.__class__.__name__}({self.name!r}, {self.type!r}, nullable={self.nullable})"
 
     def spec_sql(self) -> str:
+        """
+        The SQLite statement to create the column.
+
+        :return: An SQLIte statement.
+        """
         return f"{self.name} {self.type} {'not null' if not self.nullable else ''}".strip()
 
     @classmethod
     def from_schema(cls, name: str, schema: dict, defs: dict[str, dict] | None = None) -> Self:
+        """
+        Generate a column from a JSON schema.
+
+        :param name: The name of the column.
+        :param schema: The JSON schema of the column.
+        :param defs: The definitions in the schema.
+        :return: A ``ColumnSpec`` instance.
+        """
         defs = defs or {}
         if schema.get("$ref"):
             schema.update(defs[schema.get("$ref", "").removeprefix("#/$defs/")])
@@ -125,6 +148,13 @@ class ColumnSpec:
 
     @classmethod
     def from_model(cls, model: Type[BaseModel], ignore: list[str] | None = None) -> list[Self]:
+        """
+        Generate a list of columns from a Pydantic model.
+
+        :param model: The model to create the columns from.
+        :param ignore: A list of column names to ignore.
+        :return: A list of ``ColumnSpec`` instances.
+        """
         schema: dict = model.model_json_schema()
         ignore = ignore or []
 
