d4662bb4f5d21dc3fd5f9d871220f59f5eed9610
Matteo Campinoti <matteo.campinoti94@gmail.com>
2024-11-27 16:57:57 +0100

    database.table_keyvalue - add docstrings

--- acacore/database/table_keyvalue.py
+++ acacore/database/table_keyvalue.py
@@ -12,17 +12,32 @@ from .table import Table
 
 
 class KeysTableModel(BaseModel):
+    """The model for the rows in a key-value store table."""
+
     key: str
     value: object | None
 
 
 class KeysTable(Generic[_M]):
+    """
+    A class that represents a key-value store table in an SQLite database and allows accessing the contents with a Pydantic model.
+
+    :ivar table: The underlying ``Table`` object that handles insert/read/update/delete operations for each field.
+    :ivar model: The model the table is based on.
+    """
+
     def __init__(self, database: Connection, model: Type[_M], name: str) -> None:
+        """
+        :param database: The connection to the database.
+        :param model: The Pydantic model to create the table for.
+        :param name: The name of the table.
+        """  # noqa: D205
         self.table: Table[KeysTableModel] = Table(database, KeysTableModel, name)
         self.model: Type[_M] = model
 
     @property
     def name(self) -> str:
+        """The name of the table."""
         return self.table.name
 
     @name.setter
@@ -45,13 +60,24 @@ class KeysTable(Generic[_M]):
         self.table.insert(KeysTableModel(key=key, value=value), on_exists="replace")
 
     def create_sql(self, *, exist_ok: bool = False) -> str:
+        """Generate the SQL statement to create the table."""
         return self.table.create_sql(exist_ok=exist_ok)
 
     def create(self, *, exist_ok: bool = False):
+        """
+        Create the table in the connected database.
+
+        :param exist_ok: Whether to ignore any existing table with the same name.
+        """
         self.table.create(exist_ok=exist_ok)
         return self
 
     def set(self, obj: _M):
+        """
+        Save an object into the table.
+
+        :param obj: The object to be saved.
+        """
         self.table.insert(*(KeysTableModel(key=k, value=o) for k, o in obj.model_dump().items()), on_exists="replace")
 
     @overload
@@ -61,6 +87,12 @@ class KeysTable(Generic[_M]):
     def get(self, key: str) -> Any | None: ...  # noqa: ANN401
 
     def get(self, key: str | None = None) -> _M | Any | None:
+        """
+        Get the object stored in the table.
+
+        :param key: If given, return only the value of that field.
+        :return: The object stored in the table.
+        """
         if key is not None and key not in self.model.model_fields:
             raise AttributeError(f"{self.model.__name__!r} object has no attribute {key!r}")
 
@@ -74,6 +106,11 @@ class KeysTable(Generic[_M]):
             return obj
 
     def update(self, **kwargs: object | None):
+        """
+        Updated specific fields of the object stored in the table.
+
+        :return: The number of updated fields.
+        """
         if missing_keys := [k for k in kwargs if k not in self.model.model_fields]:
             raise AttributeError(
                 f"Fields {', '.join(map(repr, missing_keys))} do not exist in model {self.model.__name__!r}"
